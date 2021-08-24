1765d35bd6b5d9bc2292f99db09e7aa2ba1bd0cd
wyne-ybanez <wyne_01@icloud.com>
2021-08-24 23:57:01 +0100

    Add media queries for larger screen regarding about section

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -2,3 +2,4 @@ let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
